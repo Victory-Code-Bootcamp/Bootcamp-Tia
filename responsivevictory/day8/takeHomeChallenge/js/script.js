7f9d3290ca1f3c6e0b712d574af7e92905a06746
“Tia <tialee_93@hotmail.com>
2023-05-04 13:49:56 -0700

    hw

---
+++ responsivevictory/day8/takeHomeChallenge/js/script.js
@@ -0,0 +1,21 @@
+// let a = 10;
+// let b = 5;
+// let c = Math.max(a, b);
+// alert(c);
+
+// let x = Math.random();
+// alert(Math.round(x));
+
+// let x = prompt("Please enter a number: ");
+// alert(`The square root of ${x} is ${Math.sqrt(x)}`);
+
+// let date = new Date();
+// alert(`The current date is ${date}`);
+
+// let date = new Date();
+// let year = date.getFullYear();
+// alert(`The current year is ${year}`);
+
+let days = 10;
+let newDate = new Date(date.setTime(date.getTime() + days * 86400000));
+alert(`The current date plus ten days is ${newDate}`);
